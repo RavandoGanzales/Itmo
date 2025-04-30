23d644e6f7287eb5a20f9e4e7dac0ab78af7701e
RavandoGanzales <Eugeneswan239@gmail.com>
2025-04-30 20:08:07 +0300

    Add files via upload
    оба файла надо закинуть в одну папку и потом запустить .py файл

---
+++ задача 4.py
@@ -0,0 +1,28 @@
+f=open('CdSe_CdZnS Core_Shell.txt','r')
+# здесь первый столбик - длина волны, а второй - мощность излучения. Мощность является изучаемой величиной
+# код надо запустить в одной папке с этим файлом чтобы программа работала
+s=f.readlines()
+f.close()
+v=[] #здесь хранятся значения мощности
+summ=0
+amount=0
+for line in s:
+    line=line.replace('\n','')
+    line = line.replace(',', '.')
+    value=float(line.split('\t')[1])
+    v.append(value)
+    summ+=value
+    amount += 1
+average=summ/amount
+average_deviation=0
+average_square_deviation=0
+for value in v:
+    average_deviation+=(value-average)/amount
+    average_square_deviation+=((value-average)**2)/amount
+average_square_deviation=average_square_deviation**0.5
+print("Cреднее отклонение: ",average_deviation, "\nСреднеквадратическое отклонение: ", average_square_deviation)
+print("Cреднее значение мощности: ",average) # если надо
+
+
+
+
